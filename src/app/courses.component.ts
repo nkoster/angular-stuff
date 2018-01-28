6cf9f50a6af67bc3e455fbd2dbdc1b2552619965
Niels Koster <n.koster@portavita.eu>
2018-01-28 16:56:25 +0100

    Loop

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ "Title: " + getTitle() }}</h2>'
+    template: `
+        <h2>{{ "Title: " + getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">{{ course }}</li>
+        </ul>
+    `
 })
 
 export class CoursesComponent {
