cca9644be7bd00fb005656c65cb561fb65605cd6
Niels Koster <n.koster@portavita.eu>
2018-01-29 19:39:36 +0100

    Add bootstrap

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,16 +4,25 @@ import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     template: `
+        <button class="btn btn-primary">Save</button>
         <h2>{{ "Title: " + getTitle() }}</h2>
         <ul>
             <li *ngFor="let course of courses">{{ course }}</li>
         </ul>
+        <img [src]="imageUrl" />
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan">aap</td>
+            </tr>
+        </table>
     `
 })
 
 export class CoursesComponent {
     title = 'List of courses';
+    imageUrl = 'http://lorempixel.com/400/200';
     courses;
+    colSpan = 2;
     constructor(service: CoursesService) {
         this.courses = service.getCourses()
     }
