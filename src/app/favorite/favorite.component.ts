83a61f0ae5d77026481e90467dd84d8eecf91389
Niels Koster <n.koster@portavita.eu>
2018-02-01 01:52:36 +0100

    override style in component

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  styles: [
+    `
+      .glyphicon {
+        color: green
+      }
+    `
+  ]
 })
 export class FavoriteComponent implements OnInit {
   @Input() isFavorite: boolean;
