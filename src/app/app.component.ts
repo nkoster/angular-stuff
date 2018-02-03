7dbdb4d192f93baab8ad345dfd0a2c1e79ca4459
Niels Koster <n.koster@portavita.eu>
2018-02-03 16:14:23 +0100

    ngSwitchCase example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
     isLiked: true
   }
   courses = [];
+  viewMode = 'list';
   onFavoriteChanged(eventArgs: onFavoriteChangedEventArgs) {
     console.log('Favorite changed: ' + eventArgs.newValue)
   }
