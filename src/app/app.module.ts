cffd6d6f75340de3e89f8b87a636e0f6bc3556cf
Niels Koster <n.koster@portavita.eu>
2018-02-11 18:28:02 +0100

    Reactive form, first part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
@@ -12,6 +12,7 @@ import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
     declarations: [
@@ -24,11 +25,13 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
       LikeComponent,
       InputFormatDirective,
       ZippyComponent,
-      ContactFormComponent
+      ContactFormComponent,
+      SignupFormComponent
     ],
     imports: [
       BrowserModule,
-      FormsModule
+      FormsModule,
+      ReactiveFormsModule
     ],
     providers: [
       CoursesService
