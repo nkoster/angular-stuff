a3c035f6c021d83b63c4e9aeac39a8217cd0faca
Niels Koster <n.koster@portavita.eu>
2018-02-13 22:41:05 +0100

    Add custom validators

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
 
 @Component({
   selector: 'signup-form',
@@ -12,7 +13,9 @@ export class SignupFormComponent {
   form = new FormGroup({
     username: new FormControl('', [
       Validators.required,
-      Validators.minLength(3)
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpaces,
+      UsernameValidators.cannotBeShorterThanTwo
     ]),
     password: new FormControl('', Validators.required)
   })
