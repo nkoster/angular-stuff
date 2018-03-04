2d12dfce9b08c1aae381c7129483fe5fd6f1d922
Niels Koster <n.koster@portavita.eu>
2018-03-04 20:36:45 +0100

    Handle unexpected errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -15,40 +15,60 @@ export class PostsComponent implements OnInit {
 
   ngOnInit() {
     this.service.getPosts()
-    .subscribe(response => {
+    .subscribe(
+      response => {
         this.posts = response.json()
-      })
+      },
+      error => {
+        console.error(error)
+      }
+    )
   }
   
   createPost(input: HTMLInputElement) {
     let post = { title: input.value }
     input.value = '';
     this.service.createPost(post)
-      .subscribe(response => {
-        // square brackets since post is not of type :any
-        // which is an alternative way
-        post['id'] = response.json().id;
-        this.posts.splice(0, 0, post);
-        //console.log(response.json())
-      })
+    .subscribe(
+      response => {
+      // square brackets since post is not of type :any
+      // which is an alternative way
+      post['id'] = response.json().id;
+      this.posts.splice(0, 0, post);
+      //console.log(response.json())
+      },
+      error => {
+        console.error(error)
+      }
+    )
   }
 
   updatePost(post) {
     // update only a part of an object
     this.service.updatePost(post.id)
-      .subscribe(response => {
-      console.log(`response=${response.json()}`)
-    })
+    .subscribe(
+      response => {
+        console.log(`response=${response.json()}`)
+      },
+      error => {
+        console.error(error)
+      }
+    )
     // whole object
     //this.http.put(this.url, JSON.stringify(post))
   }
 
   deletePost(post) {
     this.service.deletePost(post.id)
-    .subscribe(response => {
-      let index = this.posts.indexOf(post);
-      this.posts.splice(index, 1)
-    })
+    .subscribe(
+      response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1)
+      },
+      error => {
+        console.error(error)
+      }
+    )
   }
 
 }
