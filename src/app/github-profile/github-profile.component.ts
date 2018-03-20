6683930216d60b902b118b1b3309a4ce9b85c0df
Niels Koster <n.koster@portavita.eu>
2018-03-20 23:49:02 +0100

    Update selector.

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-github-profile',
+  selector: 'github-profile',
   templateUrl: './github-profile.component.html',
   styleUrls: ['./github-profile.component.css']
 })
